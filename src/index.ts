be087728be2f754879a4b74a24ea53eaa6f13983
Dimas Knitto <143385412+dimasknitto@users.noreply.github.com>
2024-01-12 10:58:28 +0700

    feat: tambah pengujian dan versioning (#5)
    1. penambahan babel dan jest untuk pengujian.
    2. penambahan versioning menggunakan release-it.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
+import { logger } from '@knittotextile/knitto-core-backend';
 import httpServer from './app/http';
 import listener from './app/listener';
 import { connectionMysql } from './config/dbConnection';
 
-(
-	async () => {
-		try {
-			await connectionMysql.init();
-			await listener();
-			await httpServer();
-		} catch (error) {
-			console.error(error);
-		}
+async function startApp() {
+	try {
+		await httpServer();
+		await connectionMysql.init();
+		await listener();
+	} catch (error) {
+		logger.error(error);
+		throw error;
 	}
-)();
+}
+
+startApp();
+
+export default startApp;
