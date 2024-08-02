ce34d84d6429488cbfac357663864e7757b7e1e0
dikisubagjaknitto <it01.knitto@gmail.com>
2024-08-02 11:22:02 +0700

    (feat): add swagger for documentation

--- src/libs/config/index.ts
+++ src/libs/config/index.ts
@@ -9,6 +9,7 @@ export const APP_VERSION = packageJson.version || '0.0.0';
 export const NODE_ENV = process.env.NODE_ENV ?? 'development';
 export const APP_SECRET_KEY = process.env.APP_SECRET_KEY || 'secret';
 export const APP_PORT_HTTP = String(process.env.APP_PORT_HTTP) || '8000';
+export const APP_EXPOSE_DOCS = Boolean(process.env.APP_EXPOSE_DOCS) || true;
 
 export const mysqlConfig = {
 	HOST: process.env.DB_HOST_MYSQL || 'localhost',
